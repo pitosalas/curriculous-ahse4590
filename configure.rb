f54d8adfbd89544bc76e96016bf6bb35df25d03b
Pito Salas <pitosalas@gmail.com>
2013-09-16 20:11:07 -0400

    first commit

---
+++ configure.rb
@@ -0,0 +1,11 @@
+# URL for AWS Deployment of the course
+AWS_BUCKET = "ahse4590.courses.salas.com"
+
+# Local directory path for directory of topics
+TOPICS_DIR = "/mydev/curriculous-topics"
+
+# Copyright string
+COPYRIGHT_STRING = "Copyright (2013) R. Pito Salas, pitosalas@gmail.com"
+
+# Course Name
+COURSE_NAME = "AHSE 4590"
